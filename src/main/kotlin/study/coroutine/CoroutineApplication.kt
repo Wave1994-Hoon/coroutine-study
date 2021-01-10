6de83724380d1a82d814cb088ca203187ea71cb5
kwanghoon.kim <kk03187693@gmail.com>
2021-01-10 22:23:37 +0900

    initial setting

---
+++ src/main/kotlin/study/coroutine/CoroutineApplication.kt
@@ -0,0 +1,11 @@
+package study.coroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutineApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoroutineApplication>(*args)
+}
